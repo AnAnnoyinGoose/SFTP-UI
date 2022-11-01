0f6d5e063916c5cbcceed81d92e10134bd494e42
AnAnnoyinGoose <anannoyingoose@gmail.com>
2022-11-01 22:08:31 +0100

    Now it works better

--- server.js
+++ server.js
@@ -9,26 +9,21 @@ const sftp = new Client();
 const currTime = () => {
     return new Date().toLocaleString();
 }
-
 const connectSFTP = async () => {
     try {
         await sftp.connect({
             host: process.env.HOSTFTP,
             port: process.env.PORTFTP || 22,
             username: process.env.USERNAMEFTP || 'root',
-            password: process.env.PASSWORDFTP || 'GooseIsGood',
+            password: process.env.PASSWORDFTP || 'mmmMommyMilk',
         });
-        console.log("SFTP connected");
+
     } catch (err) {
         console.log(err, "catch error");
     }
 }
-
-// create a tree of all visible files in the directory
-
 const listFiles = async (username) => {
     let space = '    ';
-    let branch = '│   ';
     let tee = '├── ';
     let last = '└── ';
 
@@ -49,7 +44,7 @@ const listFiles = async (username) => {
     }, {});
 
     let result = '';
-    let print = (tree, indent = '') => {
+    let print = (tree, indent) => {
         Object.keys(tree).forEach((key, i, keys) => {
             let isLast = i === keys.length - 1;
             result += indent + (isLast ? last : tee) + key + '\n';
@@ -57,27 +52,15 @@ const listFiles = async (username) => {
 
 
     }
-    print(tree);
+    print(tree,space);
     return result;
 
 }
-
-
-// send
-
-
 const main = async (username) => {
-    if (sftp.connected) {
-        await listFiles(username).then((result) => {
+    await listFiles(username).then(
+        (result) => {
             console.log(result);
         });
-    } else {
-        await connectSFTP();
-        await listFiles(username).then((result) => {
-            console.log(result);
-        });
-
-    }
 }
 const disconnectSFTP = async () => {
     try {
@@ -87,6 +70,10 @@ const disconnectSFTP = async () => {
         console.log(err, "catch error");
     }
 }
+connectSFTP().then(() => {
+    console.log("SFTP connected " + currTime());
+});
+
 
 class User {
     constructor(username, password) {
@@ -95,21 +82,16 @@ class User {
         this.allowedSite = this.username
     }
 }
-
 const users = {
     films: new User('films', '.'),
     music: new User('music', '.'),
     books: new User('books', '.'),
     docs: new User('docs', '.')
 }
-
-
 // console.log(users); // for debugging
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({extended: true}));
-
 
 app.post("/login", (req, res) => {
     const {username, password} = req.body;
@@ -125,14 +107,31 @@ app.post("/login", (req, res) => {
     }
 });
 
-
 app.get("/disconnect", (req, res) => {
-    disconnectSFTP().then(() => {
         res.sendFile(__dirname + '/public/index.html');
-    });
+        console.log('logout success at ', currTime());
 })
 
 app.listen(process.env.PORT || 8080, () => {
     console.log("Application started and Listening on port " + process.env.PORT + " (http://localhost:" + process.env.PORT || 8080 + " ) " || 8080);
 });
+
+// on console input disconnect
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (text) {
+    if (text === 'disconnect\n') {
+        disconnectSFTP().then(() => {
+            process.exit();
+        });
+    }
+    if (text === 'reconnect\n') {
+        disconnectSFTP().then(() => {
+            connectSFTP().then(() => {
+                console.log('reconnected at ', currTime());
+            });
+        });
+    }
+});
+
 
